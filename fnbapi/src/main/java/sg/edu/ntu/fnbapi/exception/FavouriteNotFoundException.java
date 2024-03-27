b55c03711c296633d0d8c58dfc8bb27504f7136c
MT7654 <malcolmtoh@ymail.com>
2024-03-27 19:00:45 +0800

    completed exceptions

--- fnbapi/src/main/java/sg/edu/ntu/fnbapi/exception/FavouriteNotFoundException.java
+++ fnbapi/src/main/java/sg/edu/ntu/fnbapi/exception/FavouriteNotFoundException.java
@@ -1,5 +1,7 @@
 package sg.edu.ntu.fnbapi.exception;
 
-public class FavouriteNotFoundException extends RuntimeException{
-    
+public class FavouriteNotFoundException extends RuntimeException {
+    public FavouriteNotFoundException() {
+        super("Favourite not found");
+    }
 }
--- fnbapi/src/main/java/sg/edu/ntu/fnbapi/exception/GlobalExceptionHandler.java
+++ fnbapi/src/main/java/sg/edu/ntu/fnbapi/exception/GlobalExceptionHandler.java
@@ -5,14 +5,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.LocalDateTime;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RestaurantNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleRestaurantNotFoundException(RestaurantNotFoundException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), LocalDateTime.now());
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(ConsumerNotFoundException.class)
+    public ResponseEntity<String> handleConsumerNotFoundException(ConsumerNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
-  }
+
+    @ExceptionHandler(FavouriteNotFoundException.class)
+    public ResponseEntity<String> handleFavouriteNotFoundException(FavouriteNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RestaurantNotFoundException.class)
+    public ResponseEntity<String> handleRestaurantNotFoundException(RestaurantNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
